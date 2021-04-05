29374c97aa7678df43f5122d0f0248e789e725f9
Kirill Ostapenko <djkroq@gmail.com>
2021-04-05 22:43:57 +0500

    Add files via upload

--- components/navBar.vue
+++ components/navBar.vue
@@ -1,39 +1,28 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">News</a>
+    <img src="favicon.ico"/>
+    <nuxt-link active-class="active" class="nav-link" to="/">
+    <a class="navbar-brand">News</a>
+    </nuxt-link>
 
     <div class="collapse navbar-collapse">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item">
-          <nuxt-link exact no-prefetch active-class="active" class="nav-link" to="/">
-            Home
-          </nuxt-link>
-        </li>
-        <li class="nav-item">
-          <nuxt-link active-class="active" class="nav-link" to="/about">
-            About
+          <nuxt-link active-class="active" class="nav-link" to="/">
+            Posts
           </nuxt-link>
         </li>       
-        <li class="nav-item">
-          <nuxt-link active-class="active" class="nav-link" to="/login">
-            Login
-          </nuxt-link>
-        </li>
-        <li class="nav-item">
-          <a @click.prevent="logout" class="nav-link" href="#">
-            Logout
-          </a>
-        </li>
+       
+        <b-nav-form>
+      <b-form-input class="mr-sm-2" placeholder="Search"></b-form-input>
+      <b-button variant="outline-success" class="my-2 my-sm-0" type="submit">Search</b-button>
+    </b-nav-form>
       </ul>
     </div>
   </nav>
 </template>
 
 <style>
-.active:hover {
-  justify-content: center;
-
-}
 
 </style>
 
